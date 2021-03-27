a719042e62e4b32056d98ce48d57926c65888011
kimul17 <77190603+kimul17@users.noreply.github.com>
2021-03-27 19:25:27 +0700

    Add files via upload

---
+++ p4kalkulator.php
@@ -0,0 +1,20 @@
+  
+<?php
+$varT = 12;
+$varG = 1;
+
+echo "perkalian 12 <br>";
+echo ("==============");
+echo ("<br>");
+
+for ($i = 0; $i <20;$i++){
+    echo "$varT x $varG";
+    echo "  =   ";
+    echo $hasil= $varT * $varG ;
+    echo "<br>";
+
+    $varG += 2;
+}
+
+
+?>
---
+++ p4latihan9.php
@@ -0,0 +1,6 @@
+<?php
+$arr=array("senin", "selasa", "rabu", "kamis", "jumat", "sabtu", "minggu"); 
+foreach ($arr as $hari){
+echo "Hari: " . $hari . "<br>";
+}
+?>
